25c63dd3211bf505cc3ac784518f48a383403476
ashley <nnvvhh148@gmail.com>
2021-07-11 06:15:48 +0700

    just some test

--- Backend/ML/prediction.py
+++ Backend/ML/prediction.py
@@ -18,7 +18,6 @@ model.load_weights("ML\model_checkpoint_v2.h5")
 def normalize(data):
     data_mean = data.mean(axis=0)
     data_std = data.std(axis=0)
-    print(data, data_mean, data_std)
     return (data - data_mean) / data_std, data_mean[0], data_std[0]
 
 def get_data():
@@ -30,7 +29,7 @@ def get_data():
         i += 1
     df = pd.DataFrame.from_records(data)
     print(df.head())
-    print("check1")
+    df = df.loc[ : , df.columns != 'time']
     df, mean, std = normalize(df)
     
     X = np.expand_dims(df, axis=0)
